07147e91b2ef20c00126075f2a317c996e112578
Malekulashtar508 <moriswalamalikulashtar@gmail.com>
2026-04-26 12:05:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from qawafi_server.bait_analysis import BaitAnalysis
+import os
+import sys
 
 app = Flask(__name__)
 CORS(app)
+
+# clone repo if not exists
+if not os.path.exists("qawafi"):
+    os.system("git clone https://github.com/ARBML/qawafi.git")
+
+# add to path
+sys.path.append("qawafi")
+
+from qawafi_server.bait_analysis import BaitAnalysis
 
 analysis = BaitAnalysis()
 
@@ -20,3 +30,5 @@ def analyze():
 
     result = analysis.analyze(read_from_path="input.txt", override_tashkeel=True)
     return jsonify(result)
+
+app.run(host="0.0.0.0", port=10000)
